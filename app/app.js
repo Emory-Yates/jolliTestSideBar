87ee965c9628085a9772938b56803ba96fcec39d
Lloyd Moore <ldm0007@auburn.edu>
2015-04-14 23:57:09 -0500

    4/14 push to olli dev site version 1.14

--- app/app.js
+++ app/app.js
@@ -6,8 +6,7 @@ import config from './config/environment';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  //modulePrefix: config.modulePrefix,
-  modulePrefix: 'jolli',
+  modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
